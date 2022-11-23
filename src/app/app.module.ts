e6ac849049a8a2655a13eb738e9192f5b39abd7d
Anton <antonoliverkiiski@gmail.com>
2022-11-23 13:13:57 +0200

    Create new component "hero-detail"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './component/heroes/heroes.component';
+import { HeroDetailComponent } from './component/hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
